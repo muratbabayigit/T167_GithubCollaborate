c7d4bf0296ceee32d19ae79c07316c26a421b65f
HuseyinOzger <huseyinozger1507@gmail.com>
2026-01-17 21:45:17 +0300

    ilk versiyon

--- src/OrtakClass.java
+++ src/OrtakClass.java
@@ -1,5 +1,8 @@
 public class OrtakClass {
     public static void main(String[] args) {
         System.out.println("Buradan babayigit geçti");
+
+        System.out.println("hello qa");
+
     }
 }
